4a06ab453def8656f0ef3c82d2d6006600cd6379
SACOMBANK\locdh31439 <locdh31439@sacombank.com>
2020-10-05 09:24:35 +0700

    MongoDB DotNet sample

---
+++ MongoDBService.cs
@@ -0,0 +1,471 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using MongoDB.Bson.Serialization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MongoDBTest
+{
+
+    public class MongoDBService : IDisposable
+    {
+        //protected readonly ILog _log = LogManager.GetLogger("");
+
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _database;
+        protected const int MAX_LIMIT = 1000;
+
+
+        #region Constructors
+        public MongoDBService(string connectionString, string dbName)
+        {
+            try
+            {
+                _client = new MongoClient(connectionString);
+                _database = _client.GetDatabase(dbName);
+            }
+            catch (Exception ex)
+            {
+                //_log.Error(ex);
+            }
+        }
+        #endregion
+
+        #region Select
+        /// <summary>
+        /// Returns a list of the given BsonDocument filter
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collectionName"></param>
+        /// <param name="filter"></param>
+        /// <param name="limit"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<List<T>> SelectAsync<T>(string collectionName, FilterDefinition<BsonDocument> filter, int limit = MAX_LIMIT, CancellationToken cancellationToken = default)
+        {
+            var returnList = new List<T>();
+            try
+            {
+                var limit_list = limit > MAX_LIMIT ? MAX_LIMIT : limit;
+                var collection = _database.GetCollection<BsonDocument>(collectionName);
+
+                var result = await collection.Find(filter).Limit(limit).ToListAsync(cancellationToken);
+
+                foreach (var item in result)
+                {
+                    returnList.Add(BsonSerializer.Deserialize<T>(item));
+                }
+            }
+            catch (Exception ex)
+            {
+                //_log.Error(ex);
+            }
+            return returnList;
+        }
+
+        /// <summary>
+        /// Returns a list of the given key-value string
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collectionName"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="limit"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<List<T>> SelectAsync<T>(string collectionName, string key, string value, int limit = MAX_LIMIT, CancellationToken cancellationToken = default)
+        {
+            var returnList = new List<T>();
+            try
+            {
+                limit = limit > MAX_LIMIT ? MAX_LIMIT : limit;
+                var collection = _database.GetCollection<BsonDocument>(collectionName);
+                var filter = Builders<BsonDocument>.Filter.Eq(key, value);
+
+                var result = await collection.Find(filter).Limit(limit).ToListAsync(cancellationToken);
+
+                foreach (var item in result)
+                {
+                    returnList.Add(BsonSerializer.Deserialize<T>(item));
+                }
+            }
+            catch (Exception ex)
+            {
+                //_log.Error(ex);
+            }
+            return returnList;
+        }
+
+        /// <summary>
+        /// Returns a list of the given key-String and value-type(TValue)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collectionName"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="limit"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<List<T>> SelectAsync<T, TValue>(string collectionName, string key, TValue value, int limit = MAX_LIMIT, CancellationToken cancellationToken = default)
+        {
+            var returnList = new List<T>();
+            try
+            {
+                limit = limit > MAX_LIMIT ? MAX_LIMIT : limit;
+                var collection = _database.GetCollection<BsonDocument>(collectionName);
+                var filter = Builders<BsonDocument>.Filter.Eq(key, value);
+
+
+                var result = await collection.Find(filter).Limit(limit).ToListAsync(cancellationToken);
+
+                foreach (var item in result)
+                {
+                    returnList.Add(BsonSerializer.Deserialize<T>(item));
+                }
+            }
+            catch (Exception ex)
+            {
+                //_log.Error(ex);
+            }
+            return returnList;
+        }
+
+        /// <summary>
+        /// Select a single record of the given BsonDocument filter
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collectionName"></param>
+        /// <param name="filter"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<T> SelectOneAsync<T>(string collectionName, FilterDefinition<BsonDocument> filter, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var collection = _database.GetCollection<BsonDocument>(collectionName);
+                var result = await collection.Find(filter).FirstAsync(cancellationToken);
+
+                return BsonSerializer.Deserialize<T>(result);
+            }
+            catch (Exception ex)
+            {
+                //_log.Error(ex);
+            }
+            return default;
+        }
+
+        /// <summary>
+        /// Select a single record of the given key-value string
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collectionName"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<T> SelectOneAsync<T>(string collectionName, string key, string value, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var collection = _database.GetCollection<BsonDocument>(collectionName);
+                var filter = Builders<BsonDocument>.Filter.Eq(key, value);
+                var result = await collection.Find(filter).FirstAsync(cancellationToken);
+
+                return BsonSerializer.Deserialize<T>(result);
+            }
+            catch (Exception ex)
+            {
+                //_log.Error(ex);
+            }
+            return default;
+        }
+
+
+        /// <summary>
+        /// Select a single record of the given key-String and value- type(TValue)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collectionName"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<T> SelectOneAsync<T, TValue>(string collectionName, string key, TValue value, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var collection = _database.GetCollection<BsonDocument>(collectionName);
+                var filter = Builders<BsonDocument>.Filter.Eq(key, value);
+                var result = await collection.Find(filter).FirstAsync(cancellationToken);
+
+                return BsonSerializer.Deserialize<T>(result);
+            }
+            catch (Exception ex)
+            {
+                //_log.Error(ex);
+            }
+            return default;
+        }
+        #endregion
+
+        #region Insert
+        /// <summary>
+        /// InsertOneAsync a BsonDocument 
+        /// </summary>
+        /// <param name="collectionName"></param>
+        /// <param name="doc"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<bool> InsertOneAsync(string collectionName, BsonDocument doc, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var collection = _database.GetCollection<BsonDocument>(collectionName);
+                await collection.InsertOneAsync(doc, null, cancellationToken);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //_log.Error(ex);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Insert an object of any type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collectionName"></param>
+        /// <param name="doc"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<bool> InsertOneAsync<T>(string collectionName, T doc, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var collection = _database.GetCollection<BsonDocument>(collectionName);
+                await collection.InsertOneAsync(doc.ToBsonDocument(), null, cancellationToken);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //_log.Error(ex);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Insert multiple objects into a collection
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collectionName"></param>
+        /// <param name="documents"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<bool> InsertManyAsync<T>(string collectionName, IEnumerable<T> documents, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                List<BsonDocument> docs = new List<BsonDocument>();
+                for (int i = 0; i < documents.Count(); i++)
+                {
+                    docs[i] = documents.ElementAt(i).ToBsonDocument();
+                }
+                var collection = _database.GetCollection<BsonDocument>(collectionName);
+                await collection.InsertManyAsync(docs, null, cancellationToken);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //_log.Error(ex);
+            }
+            return false;
+        }
+        #endregion
+
+        #region Update & Upsert
+        /// <summary>
+        /// Update an object
+        /// </summary>
+        /// <param name="collectionName"></param>
+        /// <param name="filter"></param>
+        /// <param name="update"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<bool> UpdateOneAsync(string collectionName, FilterDefinition<BsonDocument> filter, UpdateDefinition<BsonDocument> update, bool isUpsert = false, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var collection = _database.GetCollection<BsonDocument>(collectionName);
+                await collection.UpdateOneAsync(filter, update, new UpdateOptions { IsUpsert = isUpsert }, cancellationToken);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //_log.Error(ex);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Update an object
+        /// </summary>
+        /// <param name="collectionName"></param>
+        /// <param name="key">The field name to identify the object to be updated</param>
+        /// <param name="value">The value to the identifier field</param>
+        /// <param name="update"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<bool> UpdateOneAsync<T>(string collectionName, string key, T value, UpdateDefinition<BsonDocument> update, bool isUpsert = false, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var filter = Builders<BsonDocument>.Filter.Eq(key, value);
+                var collection = _database.GetCollection<BsonDocument>(collectionName);
+                await collection.UpdateOneAsync(filter, update, new UpdateOptions { IsUpsert = isUpsert }, cancellationToken);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //_log.Error(ex);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Update an object
+        /// </summary>
+        /// <param name="collectionName"></param>
+        /// <param name="key">The field name to identify the object to be updated</param>
+        /// <param name="value">The value to the identifier field</param>
+        /// <param name="keyUpdate"></param>
+        /// <param name="valueUpdate"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<bool> UpdateOneAsync<T, TValue>(string collectionName, string key, T value, string keyUpdate, TValue valueUpdate, bool isUpsert = false, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var filter = Builders<BsonDocument>.Filter.Eq(key, value);
+                var update = Builders<BsonDocument>.Update.Set(keyUpdate, valueUpdate);
+                var collection = _database.GetCollection<BsonDocument>(collectionName);
+                await collection.UpdateOneAsync(filter, update, new UpdateOptions { IsUpsert = isUpsert }, cancellationToken);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //_log.Error(ex);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Update to multiple objects
+        /// </summary>
+        /// <param name="collectionName"></param>
+        /// <param name="key">The field name to identify the object to be updated</param>
+        /// <param name="value">The value to the identifier field</param>
+        /// <param name="keyUpdate"></param>
+        /// <param name="valueUpdate"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<bool> UpdateManyAsync<T, TValue>(string collectionName, string key, T value, string keyUpdate, TValue valueUpdate, bool isUpsert = false, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var filter = Builders<BsonDocument>.Filter.Eq(key, value);
+                var update = Builders<BsonDocument>.Update.Set(keyUpdate, valueUpdate);
+                var collection = _database.GetCollection<BsonDocument>(collectionName);
+                await collection.UpdateManyAsync(filter, update, new UpdateOptions { IsUpsert = isUpsert }, cancellationToken);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //_log.Error(ex);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Update an Array inside an object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collectionName"></param>
+        /// <param name="arrayField"></param>
+        /// <param name="list"></param>
+        /// <param name="filter"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<bool> UpdateArrayAsync<T>(string collectionName, string arrayField, List<T> list, FilterDefinition<BsonDocument> filter, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var collection = _database.GetCollection<BsonDocument>(collectionName);
+                var update = Builders<BsonDocument>.Update.PushEach(arrayField, list);
+                await collection.FindOneAndUpdateAsync<BsonDocument>(filter, update, null, cancellationToken);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //_log.Error(ex);
+            }
+            return false;
+        }
+        #endregion
+
+        #region Delete
+        /// <summary>
+        /// Remove an object from  a collection
+        /// </summary>
+        /// <param name="collectionName"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        public async Task<bool> DeleteOneAsync(string collectionName, FilterDefinition<BsonDocument> filter, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var collection = _database.GetCollection<BsonDocument>(collectionName);
+                await collection.DeleteOneAsync(filter);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //_log.Error(ex);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Remove an object from a collection
+        /// </summary>
+        /// <param name="collectionName"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public async Task<bool> DeleteOneAsync<T>(string collectionName, string key, T value, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var collection = _database.GetCollection<BsonDocument>(collectionName);
+                var filter = Builders<BsonDocument>.Filter.Eq(key, value);
+                await collection.DeleteOneAsync(filter, cancellationToken);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //_log.Error(ex);
+            }
+            return false;
+        }
+        #endregion
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,155 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var CONNECTION_STRING = "YOUR_CONNECTION_STRING";
+                var DB_NAME = "Customer";
+                var dbService = new MongoDBService(CONNECTION_STRING, DB_NAME);
+                while (true)
+                {
+
+                    Console.WriteLine("Please input step need to run: ");
+                    Console.WriteLine("0 - Insert ");
+                    Console.WriteLine("1 - Update ");
+                    Console.WriteLine("2 - FindOne ");
+                    Console.WriteLine("3 - FindMany ");
+                    Console.WriteLine("4 - Delete ");
+                    var work_step = Console.ReadLine();
+
+                    // CODE FOR INSERT MANY DOCUMENTS TO COLLECTION
+                    //for (int i = 1; i <= 1000000; i++)
+                    //{
+                    //    var document = new BsonDocument {
+                    //    {"CifNo", $"CifNo{i}" },
+                    //    {"Name",  $"Name{i}" },
+                    //    {"Accounts",  new BsonArray {
+                    //                        new BsonDocument {
+                    //                            { "AccountNo", $"AccountNo{i}_1" },
+                    //                            { "Emails", new BsonArray { $"Email{i}_1_1", $"Email{i}_1_2", $"Email{i}_1_3" } },
+                    //                            { "Devices", new BsonArray { $"Device{i}_1_1", $"Device{i}_1_2", $"Device{i}_1_3" } },
+                    //                            { "Mobiles", new BsonArray { "Mobile1_1_1", "Mobile1_1_2", "Mobile1_1_3" } }
+                    //                        },
+                    //                        new BsonDocument {
+                    //                            { "AccountNo", $"AccountNo{i}_2" },
+                    //                            { "Emails", new BsonArray { $"Email{i}_2_1", $"Email{i}_2_2", $"Email{i}_2_3" } },
+                    //                            { "Devices", new BsonArray { $"Device{i}_2_1", $"Device{i}_2_2", $"Device{i}_2_3" } },
+                    //                            { "Mobiles", new BsonArray { $"Mobile{i}_2_1", $"Mobile{i}_2_2", $"Mobile{i}_2_3" } }
+                    //                        },
+                    //                        new BsonDocument {
+                    //                            { "AccountNo", $"AccountNo{i}_3" },
+                    //                            { "Emails", new BsonArray { $"Email{i}_3_1", $"Email{i}_3_2", $"Email{i}_3_3" } },
+                    //                            { "Devices", new BsonArray { $"Device{i}_3_1", $"Device{i}_3_2", $"Device{i}_3_3" } },
+                    //                            { "Mobiles", new BsonArray { $"Mobile{i}_3_1", $"Mobile{i}_3_2", $"Mobile{i}_3_3" } }
+                    //                            }
+                    //                    }},
+                    //        { "IsSMS", i % 2 == 0 ? true : false },
+                    //        { "IsNotiMB",  i % 5 == 0 ? true : false },
+                    //        { "IsNotiSP",  i % 7 == 0 ? true : false }
+                    //    };
+                    //    collection.InsertOne(document);
+                    //    Console.WriteLine($"Insert document -- {i} -- complete!");
+                    //    Thread.Sleep(5);
+                    //}
+
+
+                    var i = 1;
+                    switch (work_step)
+                    {
+                        default: //Insert
+                            Console.WriteLine("==Insert==");
+                            Console.WriteLine("Please insert index: ");
+                            i = Int32.Parse(Console.ReadLine());
+                            var document = new BsonDocument {
+                            {"CifNo", $"CifNo{i}" },
+                            {"Name",  $"Name{i}" },
+                            {"Accounts",  new BsonArray {
+                                                new BsonDocument {
+                                                    { "AccountNo", $"AccountNo{i}_1" },
+                                                    { "Emails", new BsonArray { $"Email{i}_1_1", $"Email{i}_1_2", $"Email{i}_1_3" } },
+                                                    { "Devices", new BsonArray { $"Device{i}_1_1", $"Device{i}_1_2", $"Device{i}_1_3" } },
+                                                    { "Mobiles", new BsonArray { "Mobile1_1_1", "Mobile1_1_2", "Mobile1_1_3" } }
+                                                },
+                                                new BsonDocument {
+                                                    { "AccountNo", $"AccountNo{i}_2" },
+                                                    { "Emails", new BsonArray { $"Email{i}_2_1", $"Email{i}_2_2", $"Email{i}_2_3" } },
+                                                    { "Devices", new BsonArray { $"Device{i}_2_1", $"Device{i}_2_2", $"Device{i}_2_3" } },
+                                                    { "Mobiles", new BsonArray { $"Mobile{i}_2_1", $"Mobile{i}_2_2", $"Mobile{i}_2_3" } }
+                                                },
+                                                new BsonDocument {
+                                                    { "AccountNo", $"AccountNo{i}_3" },
+                                                    { "Emails", new BsonArray { $"Email{i}_3_1", $"Email{i}_3_2", $"Email{i}_3_3" } },
+                                                    { "Devices", new BsonArray { $"Device{i}_3_1", $"Device{i}_3_2", $"Device{i}_3_3" } },
+                                                    { "Mobiles", new BsonArray { $"Mobile{i}_3_1", $"Mobile{i}_3_2", $"Mobile{i}_3_3" } }
+                                                    }
+                                            }},
+                                { "IsSMS", i % 2 == 0 ? true : false },
+                                { "IsNotiMB",  i % 5 == 0 ? true : false },
+                                { "IsNotiSP",  i % 7 == 0 ? true : false }
+                            };
+
+
+                            var result = await dbService.InsertOneAsync("CustomerProfiles", document);
+
+
+                            Console.WriteLine($"Result: {result}");
+                            break;
+                        case "1":
+                            Console.WriteLine("==Update==");
+                            Console.WriteLine("Please insert index, keyUpdate, valueUpdate: ");
+                            i = Int32.Parse(Console.ReadLine());
+                            var keyUpdate = Console.ReadLine();
+                            var valueUpdate = Console.ReadLine();
+                            var update_field = Builders<BsonDocument>.Update.Set(keyUpdate, valueUpdate);
+
+                            var result_update = await dbService.UpdateOneAsync("CustomerProfiles", "Accounts.AccountNo", $"AccountNo{i}_3", update_field);
+
+                            
+                            Console.WriteLine($"Result: {result_update}");
+                            break;
+                        case "2":
+                            Console.WriteLine("==FindOne==");
+                            Console.WriteLine("Please insert index: ");
+                            i = Int32.Parse(Console.ReadLine());
+
+                            BsonDocument result_find_one = await dbService.SelectOneAsync<BsonDocument>("CustomerProfiles", "CifNo", $"CifNo{i}");
+                            Console.WriteLine(result_find_one);
+                            break;
+                        case "3":
+                            Console.WriteLine("==FindMany==");
+
+                            List<BsonDocument> result_find_many = await dbService.SelectAsync<BsonDocument, bool>("CustomerProfiles", "IsSMS", true, 10);
+                            Console.WriteLine(result_find_many.ToJson());
+                            break;
+                        case "4":
+                            Console.WriteLine("==Delete==");
+                            Console.WriteLine("Please insert index: ");
+                            i = Int32.Parse(Console.ReadLine());
+
+                            var check = await dbService.DeleteOneAsync("CustomerProfiles", "CifNo", $"CifNo{i}");
+                            Console.WriteLine($"Result remove CifNo{i}: {check}");
+                            break;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
